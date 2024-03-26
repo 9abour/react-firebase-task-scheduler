71678b8775674de5c72835f8c2df4b6228ccbdd7
Mohamed AbdSabour <92522994+9abour@users.noreply.github.com>
2024-03-26 04:53:54 +0200

    Update DateFilter.tsx

--- src/components/task/components/filter/DateFilter.tsx
+++ src/components/task/components/filter/DateFilter.tsx
@@ -4,7 +4,9 @@ const DateFilter = ({ value, changeValue }: DateFilterInputType) => {
 	return (
 		<button className="w-full flex-jc-c text-grey-chateau font-medium text-xs sm:text-sm">
 			<input
-				type="date"
+				type="text"
+    onfocus="(this.type='date')"
+    onblur="(this.type='text')"
 				className="input"
 				value={value}
 				onChange={changeValue}
